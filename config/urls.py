77973840c2e87aa8873f78c7b6c1674e528ba630
MersUryKippi <reginakleseva@gmail.com>
2026-04-28 13:05:19 +0300

    Этап 4: представления для просмотра данных

--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,7 @@ from exchange import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('submit/', views.submit_page, name='submit'),
+    path('secrets/', views.secret_list, name='secret_list'),
+    path('weight/<int:pk>/', views.weight_detail, name='weight_detail'),
 ]
--- exchange/views.py
+++ exchange/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from .models import SecretWeight, Secret
 
 def index(request):
     return render(request, 'index.html')
+
+def submit_page(request):
+    return render(request, 'submit.html')
+
+def secret_list(request):
+    weights = SecretWeight.objects.all().order_by('weight')
+    return render(request, 'secret_list.html', {'weights': weights})
+
+def weight_detail(request, pk):
+    weight = SecretWeight.objects.get(id=pk)
+    count = Secret.objects.filter(weight=weight).count()
+    return render(request, 'weight_detail.html', {'weight': weight, 'count': count})
